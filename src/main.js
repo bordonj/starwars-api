7cb527a43ad75f2d22f28210a133d22cffca3276
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-01 14:03:45 -0700

    clean up code

--- src/main.js
+++ src/main.js
@@ -8,52 +8,49 @@ import StarWars from './js/star-wars.js';
 
 let getElements = (response) => {
   let results;
-  if(response.data) {
+  if (response.data) {
+    console.log('getElements response.data.results', response);
     results = response.data.results;
   } else {
+    console.log('getElements response.results', response);
     results = response.results;
   }
-  
-  
   let outputStr = '';
-  console.log('results[0].name', results[0].name);
 
   if (results[0].name) {
     for (let choice of results) {
-      console.log(choice.name);
       outputStr += `<li>${choice.name}</li>`;
     }
-    console.log('outputStr', outputStr);
   } else {
     for (let choice of results) {
-      console.log(choice.title);
       outputStr += `<li>${choice.title}</li>`;
     }
-    console.log('outputStr', outputStr);
   }
   $('.list').append(outputStr);
 
 
 
 };
-async function getMore(choice) {
-  console.log('getmore choice', choice.next);
+async function getMore(choiceData) {
+  console.log('getmore choiceData', choiceData.next);
   try {
-    const results = await fetch(choice.next);
+    const results = await fetch(choiceData.next);
       if (!results.ok) {
         throw Error(results.statusText);
       }
       let parsed = await results.json();
-      console.log('parsed, results', parsed.results);
-      console.log('parsed', parsed);
+      // console.log('parsed, results', parsed.results);
+      // console.log('parsed', parsed);
       getElements(parsed);
+      if (parsed.next) {
+        getMore(parsed);
+      }
   } catch(error) {
     return error.message;
   }
 }
 async function makeApiCall(choice) {
   const response = await StarWars.get(choice);
-  console.log('starwars response', response);
   getElements(response);
   getMore(response.data);
 }
@@ -65,10 +62,7 @@ $(document).ready(() => {
     e.preventDefault();
     $('.list').text('');
     let selected = $('#dropdown').val();
-    console.log('selected', selected);
-
-    let response = makeApiCall(selected);
-    console.log('response after submitting', response);
+    makeApiCall(selected);
 
   });
 });
